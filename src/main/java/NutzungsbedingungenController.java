4af2def842ffcb4d1e015576baa9f6b766c2884f
necromizer <93487071+necromizer@users.noreply.github.com>
2022-05-24 12:43:20 +0200

    Initial commit

---
+++ src/main/java/NutzungsbedingungenController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.FileWriter;
+import java.net.URL;
+import java.util.ResourceBundle;
+import static javafx.application.Platform.exit;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author schei
+ */
+public class NutzungsbedingungenController implements Initializable {
+
+    @FXML
+    private CheckBox cb_zugestimmt;
+    @FXML
+    private Button btn_abbruch;
+    @FXML
+    private Button btn_weiter;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void checkbox_angeklickt(ActionEvent event) {
+
+        if (cb_zugestimmt.isSelected()) {
+            btn_weiter.setDisable(false);
+        } else {
+            btn_weiter.setDisable(true);
+        }
+    }
+
+    @FXML
+    private void abruchIsClicked(ActionEvent event) {
+        exit();
+    }
+
+    @FXML
+    private void weiterIsClicked(ActionEvent event) {
+        try {
+            FileWriter fw = new FileWriter("NutzungsbedingungenVorhanden.txt");
+            fw.write("true");
+            fw.close();
+            Stage s = (Stage) btn_weiter.getScene().getWindow();
+            s.close();
+            Parent root = FXMLLoader.load(getClass().getResource("Auswahl.fxml"));
+            Scene scene = new Scene(root);
+            s.setScene(scene);
+            s.show();
+            s.setTitle("Auswahl");
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
+
+    }
+
+}
---
+++ src/main/java/hi.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class hi extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("hello");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/Nutzungsbedingungen.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
