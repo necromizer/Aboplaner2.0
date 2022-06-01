0c1c66aadb41616b83f0477b2d7e54377812c36a
sebas <sebas@DESKTOP-4LFOES7>
2022-06-01 09:07:26 +0200

    Merge origin/dev into dev
    Conflicts:
    	src/main/aboplaner/java/Spieltag.java

--- src/main/java/Spieltag.java
+++ src/main/java/Spieltag.java
@@ -1,7 +1,15 @@
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -36,20 +44,60 @@ public class Spieltag {
     }
    
     //Diese Klasse gibt aus einem Startdatum, einem Enddatum und einem Spieltag die Anzahl der Spieltage zurück
-   public void meiendeisnahe(String Startdatum, String Enddatum, String Spieltag)
-   {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yyyy");
-        Startdatum = "23 01 1997";
-        Enddatum = "27 04 1997";
-       
-        LocalDate date1 = LocalDate.parse(Startdatum, dtf);
-        LocalDate date2 = LocalDate.parse(Enddatum, dtf);
-        long daysBetween = Duration.between(date1, date2).toDays();
-        System.out.println ("Days: " + daysBetween);
+   public void meiendeisnahe(String Startdatum, String Enddatum, int Wochentag)
+   {    
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+        //String Startdatum = "06 05 2022";
+        //String Enddatum = "27 05 2022";
+        //Wochentag 1 = Sonntag
+        //Wochentag 6 = Freitag
+        //int Wochentag = 6;
+        Date date1;
+        Date date2;
+        Date zwischendate;
+        ArrayList datearray = new ArrayList();
+        int i = 0;
+        
+            try {
+                date1 = myFormat.parse(Startdatum);
+                zwischendate = date1;
+                date2 = myFormat.parse(Enddatum);
+                
+                
+                long diff = date2.getTime() - date1.getTime();
+                int tage = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+                Calendar c = Calendar.getInstance();
+                
+                while (tage != 0)
+                {
+                    c.setTime(zwischendate);
+                    int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+                    if(dayOfWeek == Wochentag)
+                    {
+                        datearray.add(zwischendate) ;
+                        i++;
+                        
+                        
+                    }
+                    c.add(Calendar.DATE, 1);
+                    zwischendate = c.getTime();
+                    tage --;
+                    
+                    
+                }
+                System.out.println(datearray);
+                
+
+            } catch (ParseException ex) {
+                Logger.getLogger(Spieltag.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+    }
    }   
    
    
    
    
-}
+
 
